d3f2061ff2411a01c371c63598d8ba334981eee3
Maheen <mgul18291@gmail.com>
2026-06-16 12:49:30 -0400

    added cors policy

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import profileRoutes from "./routes/profile.ts";
 
 const app = new Hono();
+
+app.use("*", cors({
+    origin: "http://https://flashiercardswebv2.pages.dev",
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+}));
 
 // mount routes
 app.route("/api/profile", profileRoutes);
