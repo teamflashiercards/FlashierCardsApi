41935e87851762bf2650ed433584bb835db212ee
Maheen <mgul18291@gmail.com>
2026-06-16 13:06:17 -0400

    modifie cors

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,11 @@ import profileRoutes from "./routes/profile.ts";
 const app = new Hono();
 
 app.use("*", cors({
-    origin: "http://https://flashiercardswebv2.pages.dev",
+    origin: "https://flashiercardswebv2.pages.dev",
     allowHeaders: ["Content-Type", "Authorization"],
     allowMethods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
+    maxAge: 600
 }));
 
 // mount routes
