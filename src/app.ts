a126c48fa47bb4972b1d1e394f9b48ca25c93d5a
absolutefunky <aafilak1@umbc.edu>
2026-06-26 18:05:05 -0400

    forgot to mount the route in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import profileRoutes from "./routes/profile.ts";
 import cardRoutes from "./routes/card.ts";
+import deckRoutes from "./routes/deck.ts";
 
 const app = new Hono();
 
 // mount routes
 app.route("/api/profile", profileRoutes);
 app.route("/api/card", cardRoutes);
+app.route("/api/deck", deckRoutes);
 
 export default app;
