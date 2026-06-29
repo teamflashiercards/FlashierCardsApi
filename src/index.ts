7848d8d8f9a5e4be6dfd357d83b6f9fbf1431b98
Maheen <mgul18291@gmail.com>
2026-06-28 22:45:51 -0400

    trying to update dev

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { serve } from "@hono/node-server";
 import app from "./app.ts";
 import "dotenv/config";
 
+/* 
+    Description: This code is used for local development.
+    Last updated: 6/28/2026
+*/
+
 serve({ fetch: app.fetch, port: 3000 }, (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
 });
